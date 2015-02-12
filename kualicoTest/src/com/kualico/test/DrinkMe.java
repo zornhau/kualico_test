f2a9b448f4797965711e89eaf1fff8da2bce2d79
zornhau <zornhau99@gmail.com>
2015-02-12 15:54:17 -0700

    attribution

--- kualicoTest/src/com/kualico/test/DrinkMe.java
+++ kualicoTest/src/com/kualico/test/DrinkMe.java
@@ -1,5 +1,5 @@
 package com.kualico.test;
-
+//Author John Bickerstaff
 public class DrinkMe {
 	public static void main(String[] args) { 
 	      System.out.println("Hello, I'm a placeholder for the Kuali.co test");
