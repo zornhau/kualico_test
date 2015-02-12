43510191e29d9d9390db0aaedf89f4ec8fa3f866
zornhau <zornhau99@gmail.com>
2015-02-12 14:57:34 -0700

    Delete DrinkMeToStart.java

--- kualicoTest/src/com/kualico/test/DrinkMeToStart.java
+++
@@ -1,7 +0,0 @@
-package com.kualico.test;
-
-public class DrinkMeToStart {
-	public static void main(String[] args) { 
-	      System.out.println("Hello, I'm a placeholder for the Kuali.co test");
-	   }
-}
