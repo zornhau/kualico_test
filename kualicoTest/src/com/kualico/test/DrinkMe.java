ce3e524d0c5b76eb2645df5d9388fbe04a69e21e
zornhau <zornhau99@gmail.com>
2015-02-12 15:55:59 -0700

    OCD...

--- kualicoTest/src/com/kualico/test/DrinkMe.java
+++ kualicoTest/src/com/kualico/test/DrinkMe.java
@@ -2,6 +2,6 @@ package com.kualico.test;
 //Author John Bickerstaff
 public class DrinkMe {
 	public static void main(String[] args) { 
-	      System.out.println("Hello, I'm a placeholder for the Kuali.co test");
+	      System.out.println("Hello, I'm a placeholder for the KualiCo test");
 	   }
 }
