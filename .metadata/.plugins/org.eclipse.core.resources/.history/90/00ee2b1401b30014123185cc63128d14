ac9a9da784b8209324d7b0bf45ae791ef336f4e7
zornhau <zornhau99@gmail.com>
2015-02-12 21:36:00 -0700

    nothing important...

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/90/00ee2b1401b30014123185cc63128d14
@@ -0,0 +1,7 @@
+package com.kualico.test;
+
+public class DrinkMeToStart {
+	public static void main(String[] args) { 
+	      System.out.println("Hello, I'm a placeholder for the Kuali.co test");
+	   }
+}
--- kualicoTest/src/com/kualico/test/DrinkMe.java
+++ kualicoTest/src/com/kualico/test/DrinkMe.java
@@ -1,8 +1,23 @@
+import com.kualico.test.ElevatorController;
+import com.kualico.test.Elevator;
+
 package com.kualico.test;
 //Author John Bickerstaff
+
+/*
+ * PLEASE NOTE:
+ * I sent Doug Pace a screenshot of my whiteboarding of the assignment ahead of these commits.
+ * I hope that the whiteboard will count as part of the design exercise - 
+ * I tend to think best like that - pen in hand... 
+ */
 public class DrinkMe {
 	public static void main(String[] args) { 
 	      System.out.println("Hello, I'm a placeholder for the KualiCo test");
+	      
+	      //Create an ElevatorController
+	      ElevatorController c = new ElevatorController();
+	      //c.createElevators(5);
+	      
 	   }
 }
 
