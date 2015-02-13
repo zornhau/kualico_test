eae4b1786aa2a29d8eebf2bd7a9496e9307af474
zornhau <zornhau99@gmail.com>
2015-02-12 21:11:20 -0700

    First commit: Notes
    Please note that I sent Doug a picture of the whit board I worked out
    the design on before this commit.

---
+++ kualicoTest/src/com/kualico/test/Elevator.java
@@ -0,0 +1,15 @@
+package com.kualico.test;
+
+public class Elevator {
+
+}
+
+/*
+* This class represents an elevator
+* 
+* This will:
+* a. have a way of obeying all the rules that elevators must obey
+* b. be able to communicate to the Elevator Controller all that it is doing (doors open, close etc)
+*           (will need a reference to the controller.   Pass that in from the main probably)
+* c. Know when this particular elevator has passed it's service "date"  i.e. 100 trips
+*/
---
+++ kualicoTest/src/com/kualico/test/ElevatorController.java
@@ -0,0 +1,14 @@
+package com.kualico.test;
+
+public class ElevatorController {
+
+}
+
+/*
+* This class is the Elevator Controller
+* 
+* This will:
+* a. Contain a map of Elevators
+* b. Receive button pushes requesting elevators
+* c. Find the most appropriate elevator to send in response to the button push
+*/
